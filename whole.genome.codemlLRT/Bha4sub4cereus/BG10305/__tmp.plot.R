79cfbcb06d218fb0b43aeb090fa23f98900bee87
hongqin <hqinlab@gmail.com>
2013-09-19 14:50:02 -0400

    whole genome expanded 2

---
+++ whole.genome.codemlLRT/Bha4sub4cereus/BG10305/__tmp.plot.R
@@ -0,0 +1,5 @@
+library(ape); 
+  postscript("_plot.trees.ps", horizontal=F, width=8,height=8);
+  t1=read.tree("_prot.spec.bootstrapedNJ.nwk");
+  dev.off();
+  
---
+++ whole.genome.codemlLRT/myreadme.txt
@@ -0,0 +1,5 @@
+phylogeny test for whole-genome nested model test.
+
+The working folder is Bha4sub4cerus. 
+
+
